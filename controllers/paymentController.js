faa739fc498a3ecb199e3cf277a6b6580e286c82
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-02-18 11:23:33 +0100

    Recreate payment controller

---
+++ controllers/paymentController.js
@@ -0,0 +1,18 @@
+const paystack = require("../config/paystack")
+
+exports.initializePayment = async (req, res) => {
+  try {
+    const { email, amount } = req.body
+
+    const response = await paystack.post("/transaction/initialize", {
+      email,
+      amount: amount * 100
+    })
+
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    })
+  }
+}
