b5df1e1edbb3cda7c3a7c51aaa97c3ec8b4ef0cb
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-02-18 09:20:11 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const paymentRoutes = require("./routes/paymentRoutes")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/payments", paymentRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
+})
