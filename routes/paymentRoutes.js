e47221389bf7f01ba55558a9b2cf0894dc5e874a
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-02-18 10:13:45 +0100

    Create paymentRoutes.js

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+
+const { initializePayment } = require("../controllers/paymentController")
+
+router.post("/initialize", initializePayment)
+
+module.exports = router
