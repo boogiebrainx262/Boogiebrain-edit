fcb74405c82771732a97c9bd06df4f9373184c23
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-02-19 12:36:51 +0100

    server.js

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-require("dotenv").config()
+
+const paymentRoutes = require("./routes/paymentRoutes")
 
 const app = express()
 
-// Middlewares
 app.use(cors())
 app.use(express.json())
 
-// Routes
-const paymentRoutes = require("./routes/paymentRoutes")
-app.use("/api/payments", paymentRoutes)
-
-// Root route (optional but recommended)
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀")
 })
 
-// VERY IMPORTANT FOR RENDER
+app.use("/api/payments", paymentRoutes)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log("Server running on port " + PORT)
 })
