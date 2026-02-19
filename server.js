0f9f396c0f56a756f4799f7f6946a568330188a8
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-02-19 10:57:58 +0100

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import paymentRoutes from "./routes/paymentRoutes.js";
 
-const paymentRoutes = require("./routes/paymentRoutes")
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api/payments", paymentRoutes)
+app.use("/api/payments", paymentRoutes);
 
-const PORT = process.env.PORT || 5000
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT)
-})
+  console.log(`Server running on port ${PORT}`);
+});
